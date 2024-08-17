56a6e124faab9a48fceea2db31398470fd8d4daf
CarolineCoutinhoHA <carolinecoutinhoh@gmail.com>
2024-08-17 01:03:26 -0300

    mudou

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
-import logo from '../../assets/img/logo.png'
+import logo from '../../assets/img/logo.png'; // Atualize para o caminho correto
+
+import './Navbar.css'; // Importa o CSS
 
 function Navbar() {
   const navigate = useNavigate();
-  const { usuario, handleLogout } = useContext(AuthContext);
+  const { handleLogout } = useContext(AuthContext);
 
   function logout() {
     handleLogout();
@@ -14,19 +16,19 @@ function Navbar() {
   }
 
   return (
-    <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-      <div className="container flex justify-between items-center text-lg">
-        <div className="flex items-center">
-          <img src={logo} alt="SuperFarma Logo" className="w-10 h-10 mr-2" />
-          <Link to='/home' className='text-2xl font-bold uppercase hover:text-indigo-300'>SuperFarma</Link>
+    <div className='navbar'>
+      <div className='navbar-container'>
+        <div className='navbar-title-container'>
+          <img src={logo} alt="SuperFarma Logo" className='navbar-logo' />
+          <Link to='/home' className='navbar-title'>SuperFarma</Link>
         </div>
 
-        <div className='flex gap-4'>
-          <Link to='/postagens' className='nav-link'>Produtos</Link>
-          <Link to='/temas' className='nav-link'>Categorias</Link>
-          <Link to='/cadastroTema' className='nav-link'>Cadastrar Produto</Link>
-          <Link to='/perfil' className='nav-link'>Contato</Link>
-          <Link to='' onClick={logout} className='nav-link'>Sair</Link>
+        <div className='navbar-menu'>
+          <Link to='/postagens' className='navbar-item'>Produtos</Link>
+          <Link to='/temas' className='navbar-item'>Categorias</Link>
+          <Link to='/cadastroTema' className='navbar-item'>Cadastrar Produto</Link>
+          <Link to='/perfil' className='navbar-item'>Contato</Link>
+          <Link to='' onClick={logout} className='navbar-item'>Sair</Link>
         </div>
       </div>
     </div>
